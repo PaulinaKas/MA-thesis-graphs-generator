e3d6b23e9287f482c6e2716bb046a8ffeac1a4de
Paulina Kasperek <kasperek_p@onet.pl>
2019-10-02 23:41:06 +0200

    Adding "Interest rate" column

--- new.py
+++ new.py
@@ -9,7 +9,10 @@ _FIG_SIZE = (16*0.6,12*0.6)
 tbsp = pd.read_csv('tbsp.csv')
 rates = pd.read_csv('rates.csv')
 
-new = pd.DataFrame(tbsp['Date'].append(rates['Date']))
-new.drop_duplicates(inplace=True)
-new.reset_index(inplace = True, drop = True) # created 'new' dataframe with dates available for both csv files
-new['Index'] = np.nan
+appended_pd = pd.DataFrame(tbsp['Date'].append(rates['Date']))
+appended_pd.drop_duplicates(inplace=True)
+appended_pd.reset_index(inplace = True, drop = True) # created 'new' dataframe with dates available for both csv files
+appended_pd['TBSP Index'] = np.nan
+appended_pd['Interest rate'] = np.nan
+
+print(appended_pd)
