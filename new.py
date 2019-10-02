d68e86e70c835a76c26c8b323eda16cd22dbdedd
Paulina Kasperek <kasperek_p@onet.pl>
2019-10-03 01:35:12 +0200

    Refactoring of combine() funcion - now returns 1 dataframe with duplicated values of Date column

--- new.py
+++ new.py
@@ -3,8 +3,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import scipy
 
-plt.style.use('bmh')
-_FIG_SIZE = (16*0.6,12*0.6)
+#plt.style.use('bmh')
+#_FIG_SIZE = (16*0.6,12*0.6)
 
 #'2018-12-19'
 
@@ -15,12 +15,14 @@ class CombinedData():
         self.rates = pd.read_csv(rates)
 
     def combine(self):
-        combined_df = pd.DataFrame(self.tbsp['Date'].append(self.rates['Date']))
+        combined_df = pd.DataFrame(self.tbsp.append(self.rates))
         combined_df.drop_duplicates(inplace=True)
         # created 'new' dataframe with dates available for both csv files
-        combined_df.reset_index(inplace = True, drop = True)
-        combined_df['TBSP Index'] = np.nan
-        combined_df['Interest rate'] = np.nan
+        combined_df.reset_index(inplace=True, drop=True)
+        cols_to_drop = [3,4,5,6] # columns to remove
+        # removes needless columns
+        combined_df.drop(combined_df.columns[cols_to_drop],axis=1,inplace=True)
+    
         return combined_df
 
 data = CombinedData('tbsp.csv', 'rates.csv')
