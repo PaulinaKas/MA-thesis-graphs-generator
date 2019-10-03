db3dcea2c57f03ce6fc129a4f15bb3843b39bd07
Paulina Kasperek <kasperek_p@onet.pl>
2019-10-03 19:08:56 +0200

    Slimming down combine() function and create function: remove_useless_columns()

--- new.py
+++ new.py
@@ -6,8 +6,6 @@ import scipy
 #plt.style.use('bmh')
 #_FIG_SIZE = (16*0.6,12*0.6)
 
-#'2018-12-19'
-
 class CombinedData():
 
     def __init__(self, tbsp, rates):
@@ -15,15 +13,14 @@ class CombinedData():
         self.rates = pd.read_csv(rates)
 
     def combine(self):
-        combined_df = pd.DataFrame(self.tbsp.append(self.rates))
-        combined_df.drop_duplicates(inplace=True)
-        # created 'new' dataframe with dates available for both csv files
-        combined_df.reset_index(inplace=True, drop=True)
-        cols_to_drop = [3,4,5,6] # columns to remove
-        # removes needless columns
-        combined_df.drop(combined_df.columns[cols_to_drop],axis=1,inplace=True)
-    
+        combined_df = pd.DataFrame(self.tbsp.append(self.rates)).groupby('Date').first()
         return combined_df
 
+    def remove_useless_columns(self):
+        thin_combined_df = self.combine()
+        thin_combined_df.drop(thin_combined_df.columns[[2,3,4,5]],axis=1,inplace=True) # [2,3,4,5] are columns which should be removed
+        return thin_combined_df
+
 data = CombinedData('tbsp.csv', 'rates.csv')
-print(data.combine())
+data.combine()
+print(data.remove_useless_columns())
