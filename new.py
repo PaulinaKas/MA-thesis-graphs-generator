96c31ba8304bc2302fd3dd8394e9b6c7ba8f08ee
Paulina Kasperek <kasperek_p@onet.pl>
2019-10-03 21:05:36 +0200

    Adding main() function

--- new.py
+++ new.py
@@ -54,11 +54,14 @@ class Graphs(CombinedData):
         fig.tight_layout()
         plt.savefig("graph.png",bbox_inches='tight',dpi=300)
 
+def main():
+    data = CombinedData('tbsp.csv', 'rates.csv') # here are csv files which script is wotking on
+    data.combine()
+    data.remove_useless_columns()
 
-data = CombinedData('tbsp.csv', 'rates.csv') # here are csv files which script is wotking on
-data.combine()
-data.remove_useless_columns()
+    graph = Graphs('tbsp.csv', 'rates.csv') # here are csv files which script is wotking on
+    graph.prepare_lines_for_chart()
+    graph.save_chart()
 
-graph = Graphs('tbsp.csv', 'rates.csv') # here are csv files which script is wotking on
-graph.prepare_lines_for_chart()
-graph.save_chart()
+if __name__ == "__main__":
+    main()
